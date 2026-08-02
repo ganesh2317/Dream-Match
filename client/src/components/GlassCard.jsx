9c58934e63d1b736fc5a647df71b292243dff2ef
ganesh2317 <ganeshmahalatkar@gmail.com>
2026-08-02 20:25:28 +0530

    style: normalize formatting

--- client/src/components/GlassCard.jsx
+++ client/src/components/GlassCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 /**
  * @file GlassCard.jsx
  * GlassCard component providing a consistent premium glassmorphic style.
- * 
+ *
  * @param {object} props - Component props
  * @param {React.ReactNode} props.children - Child components to render inside the card
  * @param {string} [props.className] - Optional additional CSS class names
